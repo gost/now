6d23851acc1357fa8ee163add1c53dab3b2ee319
bert <btemme@hotmail.com>
2017-04-11 13:04:44 +0200

    add comments

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"time"
 )
 
+// Period struct (Start, End)
 type Period struct {
 	Start string
 	End   string
 }
 
+// UnmarshalJSON fills period object
 func (p *Period) UnmarshalJSON(buf []byte) error {
 	tmp := []interface{}{&p.Start, &p.End}
 	wantLen := len(tmp)
@@ -24,7 +26,8 @@ func (p *Period) UnmarshalJSON(buf []byte) error {
 	return nil
 }
 
-func GetPeriod(period string) Period {
+// GetPeriodFromPostgresString converts Postgres period string to object 
+func GetPeriodFromPostgresString(period string) Period {
 	var p Period
 	if err := json.Unmarshal([]byte(period), &p); err != nil {
 		log.Fatal(err)
@@ -32,18 +35,21 @@ func GetPeriod(period string) Period {
 	return p
 }
 
+// ParsePostgresTime parses postgres time to object
 func ParsePostgresTime(t string) time.Time {
 	var result, _ = time.Parse("2006-01-02 15:04:05-07", t)
 	return result
 }
 
+// TimeToIso8601 converts time object to ISO8601 object
 func TimeToIso8601(t time.Time) string {
 	var result = t.Format("2006-01-02T15:04:05.000Z")
 	return result
 }
 
+// PostgresToIso8601Period converts Postgres period format to Iso8601 format
 func PostgresToIso8601Period(period string) string {
-	var p = GetPeriod(period)
+	var p = GetPeriodFromPostgresString(period)
 	var startTime = ParsePostgresTime(p.Start)
 	var endTime = ParsePostgresTime(p.End)
 	var iso8601Period = TimeToIso8601(startTime) + "/" + TimeToIso8601(endTime)
