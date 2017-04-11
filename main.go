bdbe6606edd108cde2f96d4ddb786210c6f74b78
bert <btemme@hotmail.com>
2017-04-11 12:46:15 +0200

    renaming things

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
-package main
+package now
 
 import (
-    "fmt"
-    "encoding/json"
-    "log"
-    "time"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
 )
 
-func main() {
-    var period = `["2014-03-01 13:00:00+00","2015-05-11 15:30:00+00"]`
-    var iso8601Period = ToIso8601Period(period) 
-    fmt.Printf("%#v\n", iso8601Period)
+type Period struct {
+	Start string
+	End   string
+}
+
+func (p *Period) UnmarshalJSON(buf []byte) error {
+	tmp := []interface{}{&p.Start, &p.End}
+	wantLen := len(tmp)
+	if err := json.Unmarshal(buf, &tmp); err != nil {
+		return err
+	}
+	if g, e := len(tmp), wantLen; g != e {
+		return fmt.Errorf("wrong number of fields in Period: %d != %d", g, e)
+	}
+	return nil
+}
+
+func GetPeriod(period string) Period {
+	var p Period
+	if err := json.Unmarshal([]byte(period), &p); err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
+
+func ParsePostgresTime(t string) time.Time {
+	var result, _ = time.Parse("2006-01-02 15:04:05-07", t)
+	return result
+}
+
+func TimeToIso8601(t time.Time) string {
+	var result = t.Format("2006-01-02T15:04:05.000Z")
+	return result
+}
+
+func PostgresToIso8601Period(period string) string {
+	var p = GetPeriod(period)
+	var startTime = ParsePostgresTime(p.Start)
+	var endTime = ParsePostgresTime(p.End)
+	var iso8601Period = TimeToIso8601(startTime) + "/" + TimeToIso8601(endTime)
+	return iso8601Period
 }
