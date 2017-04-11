18ca10c62451b308c1d772341b2edc4468fddee7
bert <btemme@hotmail.com>
2017-04-11 12:10:29 +0200

    add period convertor postgres -> iso8601

--- now.go
+++ now.go
@@ -1,1 +1,51 @@
+package now
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type Period struct {
+	Start string
+	End   string
+}
+
+func (p *Period) UnmarshalJSON(buf []byte) error {
+	tmp := []interface{}{&p.Start, &p.End}
+	wantLen := len(tmp)
+	if err := json.Unmarshal(buf, &tmp); err != nil {
+		return err
+	}
+	if g, e := len(tmp), wantLen; g != e {
+		return fmt.Errorf("wrong number of fields in Period: %d != %d", g, e)
+	}
+	return nil
+}
+
+func GetPeriod(period string) Period {
+	var p Period
+	if err := json.Unmarshal([]byte(period), &p); err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
+
+func ParsePostgresTime(t string) time.Time {
+	var result, _ = time.Parse("2006-01-02 15:04:05-07", t)
+	return result
+}
+
+func TimeToIso8601(t time.Time) string {
+	var result = t.Format("2006-01-02T15:04:05.000Z")
+	return result
+}
+
+func PostgresToIso8601Period(period string) string {
+	var p = GetPeriod(period)
+	var startTime = ParsePostgresTime(p.Start)
+	var endTime = ParsePostgresTime(p.End)
+	var iso8601Period = TimeToIso8601(startTime) + "/" + TimeToIso8601(endTime)
+	return iso8601Period
+}
--- now_test.go
+++ now_test.go
@@ -1,1 +1,21 @@
+package now
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//TestIso8601 tests the ToIso8601 function
+func TestPostgresIoso8601(t *testing.T) {
+	// arrange
+	var postgresperiod = `["2014-03-01 13:00:00+00","2015-05-11 15:30:00+00"]`
+	var expectedResult = "2014-03-01T13:00:00.000Z/2015-05-11T15:30:00.000Z"
+
+	// act
+	var iso8601Period = PostgresToIso8601Period(postgresperiod)
+
+	// assert
+	assert.Equal(t, expectedResult, iso8601Period, "Iso8601 error")
+
+}
