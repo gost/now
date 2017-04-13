0371d842b0fc0c11c01b54e0691b7edc062df7fc
bert <btemme@hotmail.com>
2017-04-13 13:06:20 +0200

    add function Iso8601ToPostgresPeriodFormat

--- now_test.go
+++ now_test.go
@@ -18,3 +18,17 @@ func TestPostgresIoso8601(t *testing.T) {
 	// assert
 	assert.Equal(t, expectedResult, iso8601Period, "Iso8601 error")
 }
+
+
+// TestToPostgresPeriodFormat tests the ToPostgresPeriodFormat function
+func TestIso8601ToPostgresPeriodFormat(t *testing.T) {
+	// arrange
+	var period = "2014-03-01T13:00:00Z/2015-05-11T15:30:00Z"
+	var expectedResult = "[2014-03-01T13:00:00.000Z,2015-05-11T15:30:00.000Z]"
+
+	// act
+	var postgresPeriod = Iso8601ToPostgresPeriodFormat(period)
+
+	// assert
+	assert.Equal(t, expectedResult, postgresPeriod, "ToPostgresPeriodFormat error")
+}
